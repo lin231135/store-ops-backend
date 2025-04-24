e6d67e07e42a0b8e56aa7fac6d6646d7d4d88946
tunchxz <tun231359@uvg.edu.gt>
2025-04-23 23:48:08 -0600

    SW-125(2) Asignar permisos por módulo (Route Update)

--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { login, register } = require("../controllers/employeeController");
+const { authenticateToken, authorizeAdmin } = require("../middlewares/authMiddleware");
 
 router.post("/login", login);
-router.post("/register", register);
+router.post("/register", authenticateToken, authorizeAdmin, register);
 
 module.exports = router;
